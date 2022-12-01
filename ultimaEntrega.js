cd8365e552e5032b44ce5f7926ca7b8e8fb02e54
Anush Andino Barhich <anushbarhich@gmail.com>
2022-12-01 15:12:59 -0300

    agregando fetch y pack json

--- ultimaEntrega.js
+++ ultimaEntrega.js
@@ -7,24 +7,50 @@ mainTittle.innerText = 'Bienvenidos al Mamboreta!!'
 
 //Manipulacion de la tienda
 
-let products = [];
-let total = 0;
+// let products = [];
+// let total = 0;
 
-function add(product, price){
-    console.log(product, price)
-    products.push(product);
-    total = total + price
-    document.getElementById("checkout").innerHTML = `Pagar $${total}`
+// function add(product, price){
+//     console.log(product, price)
+//     products.push(product);
+//     total = total + price
+//     document.getElementById("checkout").innerHTML = `Pagar $${total}`
+// }
+
+// function pay(){
+//     swal.fire({
+//         text:`El total a pagar es $${total}`,
+//         confirmButtonText:'Pagar',
+//         showCancelButton: true,
+//         cancelButtonText: 'Seguir comprando'
+// })
+// }
+
+
+const cardsDiv = document.querySelector('.cards')
+
+const buscarProductos = async () => {
+    const productosFetch = await fetch('productos.json')
+    const productosJson = await productosFetch.json()
+    console.log(productosJson)
+    productosJson.forEach(prod=>{
+        const{id,title,image,price} = prod
+        cardsDiv.innerHTML += `
+        <div class="card" style="width: 18rem;">
+        <img src="${image}" class="card-img-top" alt="...">
+        <div class = "card-body">
+        <h5>${title}</h5>
+            <p>${price}</p>
+            <button id ${id} class="btn btn-primary"> AGREGAR</button>
+        </div>
+    </div>
+        `
+    })
 }
 
-function pay(){
-    swal.fire({
-        text:`El total a pagar es $${total}`,
-        confirmButtonText:'Pagar',
-        showCancelButton: true,
-        cancelButtonText: 'Seguir comprando'
-})
-}
+buscarProductos()
+
+
 
 
 //Manejo del formulario con datos del cliente
